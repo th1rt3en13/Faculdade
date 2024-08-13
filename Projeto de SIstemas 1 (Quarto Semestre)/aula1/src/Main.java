067ab3462d4a3c9515fe007f6554ac6c79b54fca
Lívian Pascoal <livianpasc@gmail.com>
2024-08-13 14:28:49 -0300

    aula1

---
+++ Projeto de SIstemas 1 (Quarto Semestre)/aula1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        char sexo = 'M';
+        byte idade =100;
+        int carros=50, casas=100;
+        short chaveAcesso= 32767;
+        long habitantesMacae = 300000;
+        float salario = 151555.90f;
+        double moeda=2.9999;
+        boolean resultado = true;
+
+        String frase= "Meu nome é Lívian";
+        final float pi =3.14f;
+
+        System.out.println("Sexo: "+sexo+", idade: "+idade+", salario: "+salario);
+        System.out.println(casas+carros);
+        System.out.println(casas+""+carros);
+
+    }
+}
