109edf6b070a9a28dc391dc5aa791bedc3ffbecb
Lívian Pascoal <livianpasc@gmail.com>
2024-08-13 20:27:25 -0300

    aula1

---
+++ Projeto de Sistemas 1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //aula1
+        Aula1 novaAula1 = new Aula1();
+        System.out.println(novaAula1.carros);
+        System.out.println(novaAula1.x); //antes de converter (float)
+        System.out.println(novaAula1.y); //depois de converter para inteiro
+
+        //funcao para leitura
+
+    }
+}
